8adbd547cd1e2a3f7fb53c3fde9d10c82683f145
mdarul <michal.darul@outlook.com>
2017-05-07 12:52:30 +0200

    Adding comments

--- Classes.py
+++ Classes.py
@@ -28,6 +28,7 @@ class History:
 
 def rand_notes(hubble_const, historical_dates):
     degrees = []
+    # we'll rand the first node in a serie, following ones' tunes will be between 0.7x and 1.3x, where x stands for previous tune
     x = rand_begin_note(hubble_const, historical_dates)
     for i in range(0, 10):
         if randint(0, 1):
@@ -43,15 +44,22 @@ def rand_notes(hubble_const, historical_dates):
 
 
 def rand_begin_note(hubble_const, historical_dates):
+    # firstly, we count up lines of "Zen of Python"
     with open("zen.txt", 'r') as f:
         lines = f.readlines()
         lines_amount = len(lines)
 
     var_list = []
+    # then we create 4 variables
     for x in range(0, 4):
+        # we choose one number from hubble_const and one from the dates
         var1 = choice(hubble_const)
         var2 = choice(historical_dates)
+        # var1 will tell as the line number
         var1 %= lines_amount
+        # var2 will be an offset
         var2 %= len(lines[var1])
+        # thanks to var1 and var2 we get a sign. Than we convert it to int and append
         var_list.append(ord(lines[var1][var2]))
+    # we sum all the values and make modulo 128, because midi tunes range is (0, 127)
     return sum(var_list) % 128
